ec6cfae3dcf26f0ad8d37b310490603892975b6e
Brando <brando90@mit.edu>
2016-05-20 18:04:56 -0400

    nearly ready to run

--- 4D_functions/main_nn1_vs_nn2.m
+++ 4D_functions/main_nn1_vs_nn2.m
@@ -1,3 +1,9 @@
+folderName = fullfile('../../../hbf_research_ml_model_library/multilayer_HModel_multivariant_regression');
+p = genpath(folderName);
+addpath(p);
+folderName = fullfile('../../../hbf_research_ml_model_library/common');
+p = genpath(folderName);
+addpath(p);
 %% target function
 h11 = @(A) (1*A(1) + 2*A(2))^2;
 h12 = @(A) (3*A(1) + 4*A(2))^3;
@@ -13,14 +19,18 @@ powertype = 'linear';
 snr = 8;
 low_x = -2;
 high_x = 2;
-nb_samples = 10000;
+nb_samples = 100;
 [X,Y] = generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype);
 D = size(X,2);
 D_out = size(Y,2);
 %% make train/test data set
 percentage_split = 0.8;
-N_train = nb_samples * percentage_split;
-N_test = nb_samples * (1 - percentage_split);
+N_train = int64(nb_samples * percentage_split);
+N_test = int64(nb_samples * (1 - percentage_split));
+X_train = X(1:N_train,:);
+Y_train = Y(1:N_train,:);
+X_test = X(1:N_test,:);
+Y_test = X(1:N_test,:);
 %% Activation funcs
 run('./activation_funcs');
 Act = relu_func;
@@ -35,20 +45,49 @@ mdl_param(2).Dim = [D_1, D_out];
 eps = 0.01;
 mdl_param(1).eps = eps;
 mdl_param(2).eps =eps;
-mdl_param.Act = Act;
-mdl_param.Act = dAct_ds;
-mdl_param.lambda = 0;
+mdl_param(1).Act = Act;
+mdl_param(1).dAct_ds = dAct_ds;
+mdl_param(1).F = 'F_NO_activation_final_layer';
+mdl_param(1).lambda = 0;
+mdl_param(2).lambda = 0;
 nn1 = make_NN_model( L, mdl_param);
 %% make 2 hidden NN model
 L=3;
 D_1 = 8;
 D_2 = 4;
-mdl_param = struct('D', cell(1,L));
+mdl_param = struct('Dim', cell(1,L), 'eps', cell(1,L) );
 mdl_param(1).Dim = [D, D_1];
 mdl_param(2).Dim = [D_1, D_2];
-mdl_param(2).Dim = [D_2, D_out];
+mdl_param(3).Dim = [D_2, D_out];
 eps = 0.01;
-epsilon(1).eps = eps;
-epsilon(2).eps =eps;
-epsilon(3).eps =eps;
-nn2 = make_NN_model( L, mdl_param, epsilon , Act, dAct_ds, lambda);
+mdl_param(1).eps = eps;
+mdl_param(2).eps =eps;
+mdl_param(3).eps =eps;
+mdl_param(1).Act = Act;
+mdl_param(1).dAct_ds = dAct_ds;
+mdl_param(2).Act = Act;
+mdl_param(2).dAct_ds = dAct_ds;
+mdl_param(1).F = 'F_NO_activation_final_layer';
+mdl_param(1).lambda = 0;
+mdl_param(2).lambda = 0;
+mdl_param(3).lambda = 0;
+nn2 = make_NN_model( L, mdl_param);
+%% train 1 hidden NN model
+sgd_errors = 1;
+nb_iterations = int64(10) % nb_iterations
+batchsize = 64
+step_size_params =  struct('eta_c', cell(1), 'eta_t', cell(1), ...
+    'AdaGrad', cell(1), 'Momentum', cell(1), ...
+    'Decaying', cell(1), 'step_size', cell(1), 'print_error_to_screen', cell(1) );
+step_size_params.print_error_to_screen = 1;
+% step_size_params.eta_c = 0.001;
+% step_size_params.eta_t = 0.001;
+% step_size_params.eta_beta = 0.001;
+% step_size_params.AdaGrad = 0;
+% step_size_params.Momentum = 0;
+step_size_params.Constant = 1;
+step_size_params.step_size = 0.01;
+step_size_params.decay_rate = 1; %if 1 its not decaying then
+[ h_mdl, iteration_errors_train, iteration_errors_test ] = multilayer_learn_HModel_explicit_b_MiniBatchSGD( X_train, Y_train, nn1, nb_iterations, batchsize, X_test,Y_test, step_size_params, sgd_errors);
+%% train 2 hidden NN model
+%[ h_mdl, iteration_errors_train, iteration_errors_test ] = multilayer_learn_HModel_explicit_b_MiniBatchSGD( X_train,Y_train, nn2, nb_iterations,batchsize, X_test,Y_test, step_size_params, sgd_errors );
--- 4D_functions/make_NN_model.m
+++ 4D_functions/make_NN_model.m
@@ -1,7 +1,9 @@
-function [ nn ] = make_NN_model( L, mdl_params)
+function [ nn ] = make_NN_model( L, mdl_param)
 run('./activation_funcs');
 nn = struct('W', cell(1,L),'b', cell(1,L),'F', cell(1,L), 'Act',cell(1,L),'dAct_ds',cell(1,L),'lambda', cell(1,L), 'beta', cell(1,L));
 %% set activation funcs
+F_func_name = mdl_param(1).F;
+nn(1).F = @F;
 for l=1:L
     if l==L
         switch F_func_name
@@ -9,22 +11,22 @@ for l=1:L
             nn(l).Act = Identity;
             nn(l).dAct_ds = dIdentity_ds;
         case 'F_activation_final_layer'
-            nn(l).Act = Act;
-            nn(l).dAct_ds = dAct_ds;
+            nn(l).Act = mdl_params(1).Act;
+            nn(l).dAct_ds = mdl_params(1).dAct_ds;
             nn(l).beta = 0;
             nn(l).lambda = lambda;
         end
     else
-        nn(l).Act = Act;
-        nn(l).dAct_ds = dAct_ds;
+        nn(l).Act = mdl_param(l).Act;
+        nn(l).dAct_ds = mdl_param(l).dAct_ds;
         nn(l).beta = 0;
-        nn(l).lambda = lambda;
+        nn(l).lambda = mdl_param(l).lambda;
     end
 end
 %% initialize
 for l=1:L
-    [D_l_1, D_l] = size(dim_layer(l).Dim);
-    nn(l).W = normrnd(0, epsilon(l).eps, [D_l_1, D_l] );
-    nn(l).b = normrnd(0, epsilon(l).eps, [1, D_l] );
+    [D_l_1, D_l] = size(mdl_param(l).Dim);
+    nn(l).W = mdl_param(l).eps * randn([D_l_1, D_l] );
+    nn(l).b = mdl_param(l).eps * randn([1, D_l] );
 end
 end
