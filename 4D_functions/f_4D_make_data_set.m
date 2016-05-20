63c0fddb76366dbe82c2d40cacc444e6f624ef28
Brando <brando90@mit.edu>
2016-05-20 15:27:36 -0400

    saving current work

--- 4D_functions/f_4D_make_data_set.m
+++ 4D_functions/f_4D_make_data_set.m
@@ -7,11 +7,17 @@ f_target(1,1).h = h11;
 f_target(1,2).h = h12;
 f_target(2,1).h = h21;
 f_target(1,1).f = @f_4D;
-%%
+%% make data set
 sigpower = 'measured';
 powertype = 'linear';
 snr = 8;
 low_x = -2;
 high_x = 2;
 nb_samples = 10000;
-generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype)
+[X,Y] = generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype);
+%% make train/test data set
+percentage_split = 0.8;
+N_train = nb_samples * percentage_split;
+N_test = nb_samples * (1 - percentage_split);
+%% save data
+% save()
