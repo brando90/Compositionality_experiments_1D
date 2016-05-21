cc1d75e1b778d38e6b704119ac43c37cd7ac430e
Brando <brando90@mit.edu>
2016-05-21 19:26:47 -0400

    working with fD8

--- 8D_functions/f_8D_make_data_set.m
+++ 8D_functions/f_8D_make_data_set.m
@@ -1,13 +1,28 @@
 restoredefaultpath;clear;clc;clear;clc;
 %% target function
+f_target = struct('h', cell(2,2), 'f', cell(2,2));
+
 h11 = @(A) (1/20)*(1*A(1) + 2*A(2))^4; % ( x1 + x2)
 h12 = @(A) (1/10)*(3*A(1) + 4*A(2))^3;
 h21 = @(A) (1/100)*(5*A(1) + 6*A(2))^2;
-f_target = struct('h', cell(2,2), 'f', cell(2,2));
 f_target(1,1).h = h11;
 f_target(1,2).h = h12;
 f_target(2,1).h = h21;
-f_target(1,1).f = @f_4D;
+
+h13 = @(A) (1/20)*(1*A(1) + 2*A(2))^4; % ( x1 + x2)
+h14 = @(A) (1/10)*(3*A(1) + 4*A(2))^3;
+h22 = @(A) (1/100)*(5*A(1) + 6*A(2))^2;
+f_target(1,3).h = h13;
+f_target(1,4).h = h14;
+f_target(2,2).h = h22;
+
+h31 = @(A) (1/10)*( (1/100)*A(1) + (1/100)*A(2) )^5;
+f_target(3,1).h = h31;
+
+% f_target(1,1).f_4D = @f_4D;
+% f_target(1,1).f_8D = @f_8D;
+% f_target(1,1).f_8D_hard_code = @f_8D_hard_code;
+f_target(1,1).f = @f_8D_hard_code;
 %% make data set
 sigpower = 'measured';
 powertype = 'linear';
@@ -15,9 +30,10 @@ snr = 8;
 low_x = -2;
 high_x = 2;
 nb_samples = 100000; %100,000
-[X,Y] = generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype);
+D = 8;
+[X,Y] = generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype, D);
 D = size(X,2);
 D_out = size(Y,2);
 %%
-save('f4D_all_data_set')
+save('f8D_all_data_set')
 beep;
--- 8D_functions/generate_data_from_function.m
+++ 8D_functions/generate_data_from_function.m
@@ -1,6 +1,6 @@
-function [ X, Y ] = generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype)
+function [ X, Y ] = generate_data_from_function( f_target, snr, low_x,high_x, nb_samples, sigpower, powertype, D)
 % sigpower = usually 'measured', powertype = usually 'linear'
-X = low_x + (high_x - low_x) * rand(nb_samples,4);
+X = low_x + (high_x - low_x) * rand(nb_samples,D);
 Y = zeros(nb_samples,1); % (N x 1)
 for n = 1:nb_samples
     xn = X(n,:);
