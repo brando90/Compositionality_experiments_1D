a00b2c8d11f68061a04b4333c5dbd68440a26d32
Brando <brando90@mit.edu>
2016-05-25 20:20:49 -0400

    fixed small error

--- 8D_functions/main_hbf1_vs_hbf2.m
+++ 8D_functions/main_hbf1_vs_hbf2.m
@@ -123,7 +123,7 @@ sgd_errors_hbf2 = 1; % record errors in SGS?
 [ step_size_params_hbf2, nb_iterations_hbf2, batchsize_hbf2 ] = step_size_HBF2( );
 %% train 1 hidden NN model
 tic
-[ hbf1, iteration_errors_train_hbf1, iteration_errors_test_hbf1 ] = multilayer_learn_HBF_MiniBatchSGD( X_train, Y_train, hbf1, nb_iterations_hbf1, batchsize_hbf1, X_test,Y_test, step_size_params_hbf1, sgd_errors_hbf1);
+%[ hbf1, iteration_errors_train_hbf1, iteration_errors_test_hbf1 ] = multilayer_learn_HBF_MiniBatchSGD( X_train, Y_train, hbf1, nb_iterations_hbf1, batchsize_hbf1, X_test,Y_test, step_size_params_hbf1, sgd_errors_hbf1);
 time_passed = toc;
 [secs_hbf1, minutes_hbf1, hours_hbf1, ~] = time_elapsed(nb_iterations_hbf1, time_passed )
 %% train 2 hidden NN model
--- 8D_functions/step_size_HBF2.m
+++ 8D_functions/step_size_HBF2.m
@@ -4,18 +4,17 @@ L = 3;
 step_size_params =  struct( 'AdaGrad', cell(1,1), 'Momentum', cell(1,1), ...
     'Decaying', cell(1,1), 'step_size', cell(1,1), ...
     'print_error_to_screen', cell(1,1) );
-step_size_params(1).print_error_to_screen = 1;
+step_size_params.print_error_to_screen = 1;
 %% constant step size
-step_size_params.Decaying = 1;
 for l=1:L
-    step_size_params.W_step_size(l).eta = 0.025;
-    step_size_params.W_step_size(l).decay_rate = 1.25; %if 1 its not decaying then
-    step_size_params.W_step_size(l).decay_frequency = 2000;
+    step_size_params.W(l).eta = 0.025;
+    step_size_params.W(l).decay_rate = 1.25; %if 1 its not decaying then
+    step_size_params.W(l).decay_frequency = 2000;
 end
 for l=1:L
-    step_size_params.Std_step_size(l).eta = 0.025;
-    step_size_params.Std_step_size(l).decay_rate = 1.25; %if 1 its not decaying then
-    step_size_params.Std_step_size(l).decay_frequency = 2000;
+    step_size_params.Std(l).eta = 0.025;
+    step_size_params.Std(l).decay_rate = 1.25; %if 1 its not decaying then
+    step_size_params.Std(l).decay_frequency = 2000;
 end
 %% nb_iterations
 nb_iterations = int64(10000);
