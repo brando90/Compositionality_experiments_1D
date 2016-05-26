282e1ed3edd4399668305f597cc8367f7003bc89
Brando <brando90@mit.edu>
2016-05-25 20:13:50 -0400

    step size code

--- 8D_functions/step_size_HBF1.m
+++ 8D_functions/step_size_HBF1.m
@@ -1,17 +1,25 @@
-function [ step_size_params_hbf1, nb_iterations_hbf1, batchsize_hbf1 ] = step_size_HBF1( )
+function [ step_size_params, nb_iterations, batchsize ] = step_size_HBF1( )
+L = 1;
 %% step-size
-step_size_params_hbf1 =  struct('eta_c', cell(1), 'eta_t', cell(1), ...
-    'AdaGrad', cell(1), 'Momentum', cell(1), ...
-    'Decaying', cell(1), 'step_size', cell(1), 'print_error_to_screen', cell(1) );
-step_size_params_hbf1.print_error_to_screen = 1;
-step_size_params_hbf1.Decaying = 1;
-step_size_params_hbf1.step_size = 0.025;
-step_size_params_hbf1.decay_rate = 1.25; %if 1 its not decaying then
-step_size_params_hbf1.mod_when = 2000;
+step_size_params =  struct( 'AdaGrad', cell(1,1), 'Momentum', cell(1,1), ...
+    'Decaying', cell(1,1), 'step_size', cell(1,1), ...
+    'print_error_to_screen', cell(1,1) );
+step_size_params.print_error_to_screen = 1;
+%% constant step size
+for l=1:L
+    step_size_params.W(l).eta = 0.025;
+    step_size_params.W(l).decay_rate = 1.25; %if 1 its not decaying then
+    step_size_params.W(l).decay_frequency = 2000;
+end
+for l=1:L
+    step_size_params.Std(l).eta = 0.025;
+    step_size_params.Std(l).decay_rate = 1.25; %if 1 its not decaying then
+    step_size_params.Std(l).decay_frequency = 2000;
+end
 %% nb_iterations
-nb_iterations_hbf1 = int64(10000);
-batchsize_hbf1 = 32;
+nb_iterations = int64(10000);
+batchsize = 4;
 %% print iteration
 factor = 100;
-step_size_params_hbf1.print_every_multiple = ceil(nb_iterations_hbf1/factor);
+step_size_params.print_every_multiple = ceil(nb_iterations/factor);
 end
--- 8D_functions/step_size_HBF2.m
+++ 8D_functions/step_size_HBF2.m
@@ -1,18 +1,26 @@
-function [ step_size_params_hbf2, nb_iterations_hbf2, batchsize_hbf2 ] = step_size_HBF2( )
+function [ step_size_params, nb_iterations, batchsize ] = step_size_HBF2( )
+L = 2;
 %% step-size
-step_size_params_hbf2 =  struct('eta_c', cell(1), 'eta_t', cell(1), ...
-    'AdaGrad', cell(1), 'Momentum', cell(1), ...
-    'Decaying', cell(1), 'step_size', cell(1), 'print_error_to_screen', cell(1) );
-step_size_params_hbf2.print_error_to_screen = 1;
-step_size_params_hbf2.Decaying = 1;
-step_size_params_hbf2.step_size = 0.01;
-step_size_params_hbf2.decay_rate = 1.5; %if 1 its not decaying then
-step_size_params_hbf2.mod_when = 2000;
+step_size_params =  struct( 'AdaGrad', cell(1,1), 'Momentum', cell(1,1), ...
+    'Decaying', cell(1,1), 'step_size', cell(1,1), ...
+    'print_error_to_screen', cell(1,1) );
+step_size_params(1).print_error_to_screen = 1;
+%% constant step size
+step_size_params.Decaying = 1;
+for l=1:L
+    step_size_params.W_step_size(l).eta = 0.025;
+    step_size_params.W_step_size(l).decay_rate = 1.25; %if 1 its not decaying then
+    step_size_params.W_step_size(l).decay_frequency = 2000;
+end
+for l=1:L
+    step_size_params.Std_step_size(l).eta = 0.025;
+    step_size_params.Std_step_size(l).decay_rate = 1.25; %if 1 its not decaying then
+    step_size_params.Std_step_size(l).decay_frequency = 2000;
+end
 %% nb_iterations
-nb_iterations_hbf2 = int64(10000);
-batchsize_hbf2 = 2;
+nb_iterations = int64(10000);
+batchsize = 4;
 %% print iteration
 factor = 100;
-step_size_params_hbf2.print_every_multiple = ceil(nb_iterations_hbf2/factor);
+step_size_params.print_every_multiple = ceil(nb_iterations/factor);
 end
-
