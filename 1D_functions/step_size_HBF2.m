eef663f4bc1e50119ff5e4202f34d3c0bfe0db1d
Brando <brando90@mit.edu>
2016-06-02 23:32:54 -0400

    commit

--- 1D_functions/step_size_HBF2.m
+++ 1D_functions/step_size_HBF2.m
@@ -16,11 +16,11 @@ if step(1).Momentum
     end
 elseif step(1).AdaGrad
     for l=1:L
-        step.W(l).G_w_eta = 10;
+        step.W(l).G_w_eta = 0.1;
         step.W(l).G_w  = zeros( size(hbf(l).W) );
     end
     for l=1:L
-        step.Std(l).G_Std_eta = 10;
+        step.Std(l).G_Std_eta = 0.1;
         step.Std(l).G_Std = zeros( size(hbf(l).beta) );
     end 
 else
@@ -28,24 +28,24 @@ else
 end
 %% decay stuff
 for l=1:L
-    step.W(l).eta = 10;
+    %step.W(l).eta = 5;
     step.W(l).decay_rate = 1.1; %if 1 its not decaying then
     step.W(l).decay_frequency = 3500;
-%     if mod(l,2) == 1
-%         step.W(l).eta = 0.000009;
-%     else
-%         step.W(l).eta = 10;
-%     end
+    if mod(l,2) == 1
+        step.W(l).eta = 0.1;
+    else
+        step.W(l).eta = 100;
+    end
 end
 for l=1:L
-    step.Std(l).eta = 100;
+    step.Std(l).eta = 0.1;
     step.Std(l).decay_rate = 1.1; %if 1 its not decaying then
     step.Std(l).decay_frequency = 2500;
 end
 %% nb_iterations
 nb_iterations = int64(6000);
-batchsize = 1000;
+batchsize = 2000;
 %% print iteration
-factor = 60;
+factor = 600;
 step.print_every_multiple = ceil(nb_iterations/factor);
 end
