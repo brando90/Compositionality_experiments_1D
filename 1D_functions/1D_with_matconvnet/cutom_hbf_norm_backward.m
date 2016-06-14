9c665dcc7c14f1806530d4c670332d43f287dcb3
Brando <brando90@mit.edu>
2016-06-14 11:26:52 -0400

    special eucledian pairwise distance derivative projection coded

--- 1D_functions/1D_with_matconvnet/cutom_hbf_norm_backward.m
+++ 1D_functions/1D_with_matconvnet/cutom_hbf_norm_backward.m
@@ -6,8 +6,12 @@ P = squeeze(p); % (D^(l) x M)
 X = squeeze(X)'; % (M x D^(l-1))
 p_sum = sum(P,1)'; % (M x 1) = (1 x M)'= sum(D^(l) x M)'
 XP = bsxfun(@times, X, p_sum); % (M x D^(l-1)) =  (M x D^(l-1)) .x (M x 1)
-dx = (-2*S)*(XP - W' * P); % (M x D^(l-1)) = (M x D^(l-1)) - (M x D^(l)) x (D^(l) x D^(l-1))
+WP = (W * P)';
+dx = (-2*S)*(XP - WP); % (M x D^(l-1)) = (M x D^(l-1)) - (M x D^(l)) x (D^(l) x D^(l-1))
 [D_l_1, M] = size(dx);
 dzdx = zeros(1,1,D_l_1,M); % TODO add singleton dim
 dzdx(1,1,:,:) = dx;
+%% TODO
+dzdw = nan;
+dzds = nan;
 end
